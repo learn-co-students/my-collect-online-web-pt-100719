eff1e2f442da2d87b0a77d3e0656e730fc84f60f
Phil Billingsby <paulbillingsby@gmail.com>
2019-10-16 13:37:10 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,11 @@ def my_collect(array)
   count = 0
   new_arr = []
   while count < array.length
-    new_arr.push(yield(array[count]))
+    new_arr << yield(array[count])
+    count += 1
   end
+  new_arr
 end
 
-my_collect(["Dave", "Terry"])
+my_collect(["Dave", "Terry"]) {|name| name.split(" ").first}
 
