60994c7410d3ab7863624d535c67239550fc2b3c
Derric Fragosa <fderric@gmail.com>
2019-10-19 20:41:20 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,8 @@ def my_collect(array)
   collected = []
   counter = 0 
   while counter < array.length 
-  collected << array[counter]
+  collected << yield(array[counter])
+  counter +=1 
    end 
    collected 
 end 
