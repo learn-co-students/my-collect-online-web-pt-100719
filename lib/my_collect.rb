0c2c7930e1fe3f29fed71ba79c0c91261ec9b12e
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-19 15:15:59 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,11 @@
+require 'pry'
 def my_collect(array)
   i = 0
   new_array = []
   while i < array.length
-  yield(array[i])
-  new_array << array[i]
+  new_array << yield(array[i])
   i += 1
   end
   new_array
 end
-
-my_collect(array) {|x| x.capitalize}
 
